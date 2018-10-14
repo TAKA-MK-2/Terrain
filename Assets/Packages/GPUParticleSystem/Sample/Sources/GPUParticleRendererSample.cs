c980e3cfebe657bbff7d5688bc5f37ea616d1ea1
TAKA-PC\TAKA <takamk2game@gmail.com>
2018-10-15 00:55:21 +0900

    GPUParticleのサンプルを追加

---
+++ Assets/Packages/GPUParticleSystem/Sample/Sources/GPUParticleRendererSample.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPUParticleRendererSample : GPUParticleRendererBase<GPUParticleData> {
+
+    protected override void SetMaterialParam()
+    {
+        material.SetBuffer("_Particles", particleBuffer);
+        material.SetBuffer("_ParticleActiveList", activeIndexBuffer);
+
+        material.SetPass(0);
+    }
+
+}
