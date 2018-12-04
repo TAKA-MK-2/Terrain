d0564f40e5843e1202ed3832e39c8b73efe94dae
ALIEN-PC <taka1122judo@gmail.com>
2018-12-04 16:22:20 +0900

    Mesh変形

--- Assets/Scripts/GenerateTerrain.cs
+++ Assets/Scripts/GenerateTerrain.cs
@@ -6,9 +6,9 @@ public class GenerateTerrain : MonoBehaviour
     // 定数
     #region define
     // ComputeShaderのスレッド数
-    protected const int THREAD_NUM = (8);
+    protected const int THREAD_NUM = (32);
     //フィールドのサイズ
-    private const float FIELD_SIZE = (20f);
+    private const float FIELD_SIZE = (100.0f);
     #endregion
 
     // エディター上で設定する変数
@@ -16,34 +16,28 @@ public class GenerateTerrain : MonoBehaviour
     // コンピュートシェーダー
     [SerializeField] ComputeShader _computeShader;
     // 1辺の頂点数
-    [SerializeField] [Range(8, 256)] int _numVertices = 8;
+    [SerializeField] [Range(256, 1024)] int _numVertices = 256;
     // 高さ
-    [SerializeField] [Range(1.0f, 10.0f)] float _height = 5.0f;
+    [SerializeField] [Range(0.0f, 100.0f)] float _height = 20.0f;
     // 滑らかさ
-    [SerializeField] [Range(0.1f, 1f)] float _smoothness = 0.5f;
+    [SerializeField] [Range(0.001f, 1f)] float _smoothness = 0.5f;
     // ノイズの取得位置
     [SerializeField] [Range(0.0f, 1000.0f)] float _offsetX = 0.0f;
     [SerializeField] [Range(0.0f, 1000.0f)] float _offsetY = 0.0f;
-    // 生成後に割り当てるマテリアル
-    [SerializeField] Material _material;
     #endregion
 
     // メンバ変数
     #region member variable
     // 頂点と頂点の距離
     private float m_distance;
-    //// 頂点数
-    //private int m_numVertices;
-    //// 頂点座標
-    //private List<Vector3> m_vertices;
-    //// 頂点データバッファ
-    //private ComputeBuffer m_vertexBuffer;
-    //// メインカーネル
-    //private int m_mainKernelID;
-    //// スレッドグループ数
-    //private int m_numThreadGroups;
-    //// Mesh
-    //private List<Mesh> m_meshs;
+    // 頂点数
+    private int m_numVertices;
+    // 頂点データバッファ
+    private ComputeBuffer m_vertexBuffer;
+    // メインカーネル
+    private int m_mainKernelID;
+    // スレッドグループ数
+    private int m_numThreadGroups;
 
     // インスタンスのメッシュ
     public Mesh _instanceMesh;
@@ -52,8 +46,6 @@ public class GenerateTerrain : MonoBehaviour
 
     // インスタンス数
     private int m_numInstances;
-    // 座標
-    private ComputeBuffer m_positionBuffer;
     // 引数
     private ComputeBuffer m_argsBuffer;
     // 引数
@@ -73,16 +65,13 @@ public class GenerateTerrain : MonoBehaviour
         // 初期化処理
         _numVertices = Mathf.CeilToInt(_numVertices / (float)THREAD_NUM) * THREAD_NUM;
         m_distance = FIELD_SIZE / _numVertices;
-        //m_numVertices = _numVertices * _numVertices;
-        //m_vertices = new List<Vector3>();
-        //m_vertexBuffer = new ComputeBuffer(m_numVertices, System.Runtime.InteropServices.Marshal.SizeOf(typeof(Vector3)));
-        //m_mainKernelID = _computeShader.FindKernel("CSMain");
-        //m_numThreadGroups = _numVertices / THREAD_NUM;
-        //m_meshs = new List<Mesh>();
+        m_numVertices = _numVertices * _numVertices;
+        m_vertexBuffer = new ComputeBuffer(m_numVertices, System.Runtime.InteropServices.Marshal.SizeOf(typeof(Vector3)));
+        m_mainKernelID = _computeShader.FindKernel("CSMain");
+        m_numThreadGroups = _numVertices / THREAD_NUM;
 
         m_numInstances = (_numVertices - 1) * (_numVertices - 1);
         m_argsBuffer = new ComputeBuffer(1, m_args.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
-        m_positionBuffer = new ComputeBuffer(m_numInstances, System.Runtime.InteropServices.Marshal.SizeOf(typeof(Vector4)));
         // メッシュの頂点数
         uint numIndices = (_instanceMesh != null) ? (uint)_instanceMesh.GetIndexCount(0) : 0;
         m_args[0] = numIndices;
@@ -92,101 +81,41 @@ public class GenerateTerrain : MonoBehaviour
         m_argsBuffer.SetData(m_args);
     }
 
-    void UpdateBuffers()
+    void CalculateVertex()
     {
-        int kernelID = _computeShader.FindKernel("CSMain2");
-        _computeShader.SetBuffer(kernelID, "_positions", m_positionBuffer);
+        // バッファを設定
+        _computeShader.SetBuffer(m_mainKernelID, "_vertices", m_vertexBuffer);
+
+        // 変数を設定
         _computeShader.SetInt("_numVertice", _numVertices);
         _computeShader.SetFloat("_distance", m_distance);
         _computeShader.SetFloat("_height", _height);
         _computeShader.SetFloat("_smoothness", _smoothness * _numVertices);
         _computeShader.SetVector("_offset", new Vector2(_offsetX, _offsetY));
-        _computeShader.Dispatch(kernelID, _numVertices - 1, 1, _numVertices - 1);
+
+        // コンピュートシェーダーを実行する
+        _computeShader.Dispatch(m_mainKernelID, m_numThreadGroups, 1, m_numThreadGroups);
     }
 
-    //void RegenerateMesh()
-    //{
-    //    // 頂点の計算
-    //    CalculateVertex();
-
-    //    // メッシュの再設定
-    //    ResettingMesh();
-    //}
-
-    //void CalculateVertex()
-    //{      
-    //    // バッファを設定
-    //    _computeShader.SetBuffer(m_mainKernelID, "_vertices", m_vertexBuffer);
-
-    //    // 変数を設定
-    //    _computeShader.SetInt("_numVertice", _numVertices);
-    //    _computeShader.SetFloat("_distance", m_distance);
-    //    _computeShader.SetFloat("_height", _height);
-    //    _computeShader.SetFloat("_smoothness", _smoothness * _numVertices);
-    //    _computeShader.SetVector("_offset", new Vector2(_offsetX, _offsetY));
-
-    //    // コンピュートシェーダーを実行する
-    //    _computeShader.Dispatch(m_mainKernelID, m_numThreadGroups, 1, m_numThreadGroups);
-
-    //    // バッファからデータを受け取る
-    //    Vector3[] vertexData = new Vector3[m_numVertices];
-    //    m_vertexBuffer.GetData(vertexData);
-
-    //    // 頂点配列にデータを格納する
-    //    m_vertices.Clear();
-    //    m_vertices.AddRange(vertexData);
-    //}
-
-    //void ResettingMesh()
-    //{
-    //    m_meshs.Clear();
-    //    // 三角形の頂点番号
-    //    int[] triangles = { 0, 2, 1, 2, 3, 1 };
-    //    for (int z = 0; z < _numVertices - 1; z++)
-    //    {
-    //        for (int x = 0; x < _numVertices - 1; x++)
-    //        {
-    //            List<Vector3> vertices = new List<Vector3>();
-    //            // 基準の頂点の識別番号
-    //            int index = z * _numVertices + x;
-    //            // 左上
-    //            vertices.Add(m_vertices[index]);
-    //            // 右上
-    //            vertices.Add(m_vertices[index + 1]);
-    //            // 左下
-    //            vertices.Add(m_vertices[index + _numVertices]);
-    //            // 右下
-    //            vertices.Add(m_vertices[index + _numVertices + 1]);
-    //            // 新しいMeshを生成
-    //            Mesh mesh = new Mesh();
-    //            // メッシュの頂点の再割り当て
-    //            mesh.SetVertices(vertices);
-    //            mesh.SetTriangles(triangles, 0);
-    //            // メッシュの法線の再計算
-    //            mesh.RecalculateNormals();
-    //            m_meshs.Add(mesh);
-    //        }
-    //    }
-    //}
-
-    //void RenderTerrain()
-    //{
-    //    foreach (Mesh mesh in m_meshs)
-    //    {
-    //        Graphics.DrawMesh(mesh, transform.position, transform.rotation, _material, 0);
-    //    }
-    //}
+    void RenderTerrain()
+    {
+        _instanceMaterial.SetInt("_numInstances", m_numInstances);
+        _instanceMaterial.SetInt("_numVertices", _numVertices);
+        _instanceMaterial.SetFloat("_distance", m_distance);
+        _instanceMaterial.SetBuffer("_vertices", m_vertexBuffer);
+        Graphics.DrawMeshInstancedIndirect(_instanceMesh, 0, _instanceMaterial, new Bounds(Vector3.zero, new Vector3(100.0f, 100.0f, 100.0f)), m_argsBuffer);
+    }
 
     void ReleaseBuffer()
     {
-        //if (m_vertexBuffer != null)
-        //{
-        //    m_vertexBuffer.Release();
-        //}
-        if (m_positionBuffer != null)
+        if (m_vertexBuffer != null)
         {
-            m_positionBuffer.Release();
+            m_vertexBuffer.Release();
         }
+        //if (m_positionBuffer != null)
+        //{
+        //    m_positionBuffer.Release();
+        //}
         if (m_argsBuffer != null)
         {
             m_argsBuffer.Release();
@@ -198,23 +127,19 @@ public class GenerateTerrain : MonoBehaviour
     {
         ReleaseBuffer();
         Initialize();
-        UpdateBuffers();
-        //RegenerateMesh();
+        CalculateVertex();
     }
 
     void Start()
     {
         ReleaseBuffer();
         Initialize();
-        UpdateBuffers();
-        //RegenerateMesh();
+        CalculateVertex();
     }
 
     void OnRenderObject()
     {
-        _instanceMaterial.SetBuffer("positionBuffer", m_positionBuffer);
-        Graphics.DrawMeshInstancedIndirect(_instanceMesh, 0, _instanceMaterial, new Bounds(Vector3.zero, new Vector3(100.0f, 100.0f, 100.0f)), m_argsBuffer);
-        //RenderTerrain();
+        RenderTerrain();
     }
 
     void OnDestroy()
--- Assets/Shaders/Terrain.shader
+++ Assets/Shaders/Terrain.shader
@@ -1,5 +1,9 @@
 ﻿Shader "Custum/Terrain"
 {
+	Properties
+	{
+		_MainTex("Texture", 2D) = "white" {}
+	}
 
 	SubShader
 	{
@@ -14,31 +18,46 @@
 			#pragma target 4.5
 
 			#include "UnityCG.cginc"
+			#include "Libs/Quaternion.cginc"
+
+			sampler2D _MainTex;
 
 			StructuredBuffer<float4> positionBuffer;
+			StructuredBuffer<float3> _vertices;
+			int _numInstances;
+			int _numVertices;
+			float _distance;
 
 			struct v2f
 			{
 				float4 pos : SV_POSITION;
+				float2 uv : TEXCOORD0;
 				float4 color: COLOR;
 			};
 
 			v2f vert(appdata_full v, uint instanceID : SV_InstanceID)
 			{
-				float4 data = positionBuffer[instanceID];
-				float3 localPosition = v.vertex.xyz * data.w;
-				float3 worldPosition = data.xyz + localPosition;
-
+				int index = instanceID + (v.texcoord.y * _numVertices + v.texcoord.x) + (instanceID / (_numVertices - 1));
+				float3 vertex = _vertices[index];
+				//float4 position = positionBuffer[instanceID];
+				//float3 localPosition = v.vertex.xyz * _distance;
+				//float4 q = quaternion(float3(1, 0, 0), 90);
+				//localPosition = rotateWithQuaternion(localPosition, quaternion(float3(1, 0, 0), 90));
+				//float3 worldPosition = vertex + localPosition;
+				vertex = rotateWithQuaternion(vertex, quaternion(float3(1, 0, 0), 0));
 				v2f o;
-				o.pos = mul(UNITY_MATRIX_VP, float4(worldPosition, 1.0f));
-
-				o.color = float4((float)(instanceID % 256) / 255.0f, 0, 0, 1);
+				o.pos = mul(UNITY_MATRIX_VP, float4(vertex, 1.0f));
+				o.uv = v.texcoord;
+				float col = instanceID / (float)_numInstances;
+				o.color = float4(col, col, col, 1);
 
 				return o;
 			}
 
 			fixed4 frag(v2f i) : SV_Target
 			{
+				//fixed4 col = tex2D(_MainTex, i.uv) * i.color;
+				//return col;
 				return i.color;
 			}
 
